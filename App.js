fc8b05e1bd8042d29566b3f32e3f5824baeec339
unknown <titusdavid39@gmail.com>
2023-11-15 02:23:05 +0100

    2nd

--- App.js
+++ App.js
@@ -1,64 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-// import { SafeAreaView } from 'react-native-safe-area-view';
-import Date from './component/Date';
-import Box from './component/Box';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import Date from "./component/Date";
+import Box from "./component/Box";
 
 export default function App() {
   return (
-    // <SafeAreaView>  
-    <ScrollView>
-    <View style={styles.container}>
-    <StatusBar style="auto" />
-    <Text> 
-    <Date number={1} month="Sep" />
-    <Box />
-    </Text>
-    <Text> 
-    <Date number={2} month="Jan" />
-    <Box />
-    </Text>
-    <Text> 
-    <Date number={3} month="Aug" />
-    <Box />
-    </Text>
-    <Text> 
-    <Date number={4} month="Dec" />
-    <Box />
-    </Text>
-    <Text> 
-    <Date number={5} month="Jul" />
-    <Box />
-    </Text>
-    <Text> 
-    <Date number={6} month="Oct" />
-    <Box />
-    </Text>
-    <Text> 
-    <Date number={7} month="Sep" />
-    <Box />
-    </Text>
-    <Text> 
-    <Date number={8} month="Jan" />
-    <Box />
-    </Text>
-    </View>
-    </ScrollView>
-    // </SafeAreaView>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Text>
+            <Date number={1} month="Sep" />
+            <Box />
+          </Text>
+          <Text>
+            <Date number={2} month="Jan" />
+            <Box />
+          </Text>
+          <Text>
+            <Date number={3} month="Aug" />
+            <Box />
+          </Text>
+          <Text>
+            <Date number={4} month="Dec" />
+            <Box />
+          </Text>
+          <Text>
+            <Date number={5} month="Jul" />
+            <Box />
+          </Text>
+          <Text>
+            <Date number={6} month="Oct" />
+            <Box />
+          </Text>
+          <Text>
+            <Date number={7} month="Sep" />
+            <Box />
+          </Text>
+          <Text>
+            <Date number={8} month="Jan" />
+            <Box />
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
-    backgroundColor: '#CCC',
+    backgroundColor: "#CCC",
     padding: 10,
     gap: 10,
     marginTop: 20,
-    flex : 1,
-    flexDirection: 'column',
-    justifyContent: 'center', 
-    alignItems: 'center',
-  },   
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
